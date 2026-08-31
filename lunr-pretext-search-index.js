45e84bd86b5429b651f9165480471ac8182463d7
batchison <batchison@framingham.edu>
2026-08-31 14:39:37 -0400

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,131 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "colophon",
+  "level": "1",
+  "url": "colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  "
+},
+{
+  "id": "chapter_integration",
+  "level": "1",
+  "url": "chapter_integration.html",
+  "type": "Chapter",
+  "number": "1",
+  "title": "Integration",
+  "body": " Integration      Review Exercises    Evaluate the given indefinite integral.                                                                  Find the general antiderivative of the given function.                                                                                               "
+},
+{
+  "id": "ex-definite-integral-review-1",
+  "level": "2",
+  "url": "exercises-integration-review.html#ex-definite-integral-review-1",
+  "type": "Exercise",
+  "number": "1.1",
+  "title": "",
+  "body": "              "
+},
+{
+  "id": "ex-definite-integral-review-2",
+  "level": "2",
+  "url": "exercises-integration-review.html#ex-definite-integral-review-2",
+  "type": "Exercise",
+  "number": "1.2",
+  "title": "",
+  "body": "              "
+},
+{
+  "id": "ex-definite-integral-review-3",
+  "level": "2",
+  "url": "exercises-integration-review.html#ex-definite-integral-review-3",
+  "type": "Exercise",
+  "number": "1.3",
+  "title": "",
+  "body": "              "
+},
+{
+  "id": "ex-definite-integral-review-4",
+  "level": "2",
+  "url": "exercises-integration-review.html#ex-definite-integral-review-4",
+  "type": "Exercise",
+  "number": "1.4",
+  "title": "",
+  "body": "              "
+},
+{
+  "id": "ex-riemann-review-1",
+  "level": "2",
+  "url": "exercises-integration-review.html#ex-riemann-review-1",
+  "type": "Exercise",
+  "number": "1.5",
+  "title": "",
+  "body": "              "
+},
+{
+  "id": "ex-riemann-review-2",
+  "level": "2",
+  "url": "exercises-integration-review.html#ex-riemann-review-2",
+  "type": "Exercise",
+  "number": "1.6",
+  "title": "",
+  "body": "              "
+},
+{
+  "id": "ex-riemann-review-3",
+  "level": "2",
+  "url": "exercises-integration-review.html#ex-riemann-review-3",
+  "type": "Exercise",
+  "number": "1.7",
+  "title": "",
+  "body": "              "
+},
+{
+  "id": "ex-riemann-review-4",
+  "level": "2",
+  "url": "exercises-integration-review.html#ex-riemann-review-4",
+  "type": "Exercise",
+  "number": "1.8",
+  "title": "",
+  "body": "              "
+},
+{
+  "id": "ex-riemann-review-5",
+  "level": "2",
+  "url": "exercises-integration-review.html#ex-riemann-review-5",
+  "type": "Exercise",
+  "number": "1.9",
+  "title": "",
+  "body": "              "
+},
+{
+  "id": "ex-riemann-review-6",
+  "level": "2",
+  "url": "exercises-integration-review.html#ex-riemann-review-6",
+  "type": "Exercise",
+  "number": "1.10",
+  "title": "",
+  "body": "              "
+},
+{
+  "id": "selected-answers",
+  "level": "1",
+  "url": "selected-answers.html",
+  "type": "Appendix",
+  "number": "A",
+  "title": "Answers to Selected Exercises",
+  "body": " Answers to Selected Exercises  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
